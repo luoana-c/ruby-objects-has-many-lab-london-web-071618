fa180d16dc5f196ec581e15f613da0f64952dc71
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-20 10:40:48 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,29 @@
-class Author 
-  attr_accessor :name
-  def initialize(name)
-    @name = name 
-  end 
-end 
+# require 'pry'
+# require_relative 'song.rb'
+
+class Author
+  attr_accessor :name, :posts, :all
+  @@all = []
+  def initialize(name=nil)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@all << post
+
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    add_post(post)
+  end
+
+  def self.post_count
+    @@all.count
+  end
+
+end
+
