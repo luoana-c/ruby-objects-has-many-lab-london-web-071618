47de74c916170ec90b15d0dfbb38c3cc4d2df252
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-20 10:41:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,20 @@
-class Song 
-  attr_accessor :title, :artist
-  def initialize(title)
-    @title = title 
-  end 
-end 
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist == nil
+      nil
+    else
+      artist.name
+    end
+  end
+end
